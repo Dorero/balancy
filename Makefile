e3ba70a55ccd3dcdc93871adc69c134d10ca86ff
Ilya Dyudyaev <domenoer@gmail.com>
2023-06-06 19:53:07 +0300

    Fix schema and makefile

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ BINARY_NAME=balancy
 default: build
 
 build:
-	GOARCH=amd64 GOOS=linux go build -o ./target/$(BINARY_NAME) src/main.go
+	GOARCH=amd64 GOOS=linux go build -o ./target/$(BINARY_NAME) main.go
 
 insert_tables:
 	PGPASSWORD=${DB_PASSWORD} psql -h db -p 5432 -U ${DB_USER} -d ${DB_NAME} -f schema.sql
--- schema.sql
+++ schema.sql
@@ -1,24 +1,54 @@
-create table accounts
+create table balance
 (
-    id      serial
-        primary key,
-    user_id integer           not null,
-    money   integer default 0 not null
+    id      integer default nextval('accounts_id_seq'::regclass) not null
+        constraint accounts_pkey
+            primary key,
+    user_id integer                                              not null,
+    money   numeric                                              not null
 );
 
-alter table accounts
+alter table balance
     owner to postgres;
 
 create table reserves
 (
     id         serial
         primary key,
-    user_id    integer not null,
-    id_service integer not null,
-    id_order   integer not null,
-    money      integer not null
+    amount     numeric not null,
+    order_id   bigint,
+    service_id bigint,
+    user_id    bigint
 );
 
 alter table reserves
     owner to postgres;
+
+create table payments
+(
+    id         bigserial
+        primary key,
+    balance_id bigint
+        constraint payments_balance_id_fk
+            references balance
+            on delete cascade,
+    amount     numeric not null
+);
+
+alter table payments
+    owner to postgres;
+
+create table unreserves
+(
+    id         serial
+        primary key,
+    amount     numeric,
+    order_id   bigint,
+    service_id integer,
+    user_id    bigint
+);
+
+alter table unreserves
+    owner to postgres;
+
+
 
